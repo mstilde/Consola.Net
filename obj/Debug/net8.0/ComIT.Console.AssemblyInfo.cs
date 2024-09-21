fa2dede9d819396115ecbc3c1508278b2016d64c
mstilde <matiasemanuelbuttari@hotmail.com>
2024-09-21 01:06:38 -0300

    ejercicios poo 1

--- obj/Debug/net8.0/ComIT.Console.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComIT.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComIT.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10008932804629b7a58d6cc9fb80b05c9302ab47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860883caa340e4f34d2529eccfa505c13dee4ae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComIT.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComIT.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
